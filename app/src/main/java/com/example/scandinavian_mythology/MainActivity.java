8bdde21fda7018e191159e8ce4ab835f7d9ab567
mobile <mobile@itcube.win>
2020-02-15 16:34:18 +0500

    init

---
+++ app/src/main/java/com/example/scandinavian_mythology/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.scandinavian_mythology;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.division_list);
+        listView.setOnItemClickListener();
+
+
+    }
+}
